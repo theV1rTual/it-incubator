b900d2f69fdd91964870751a617ab4fd0b880f08
theV1rTual <arystandev@gmail.com>
2025-09-06 20:07:25 +0500

    test

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
 import express, { Request, Response } from 'express'
-import { productsRouter } from './routers/products-router';
-import { runDb } from './repositories/db';
+import { productsRouter } from './routers/products-router'
+import { runDb } from './repositories/db'
 
 const app = express()
-const port = process.env.PORT || 5000
 
-const parserMiddleware = express.json()
-app.use(parserMiddleware)
+// Готовим подключение к БД один раз:
+const ready = runDb()
 
+app.use(express.json())
+// Ждём БД перед любым ручкой:
+app.use(async (_req, _res, next) => { await ready; next() })
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
   res.send('Hello Samurai')
 })
 
 app.use('/products', productsRouter)
 
-const startApp = async () => {
-  await runDb()
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-}
-
-startApp()
+// ❗ НИКАКОГО app.listen на Vercel
+export default app
