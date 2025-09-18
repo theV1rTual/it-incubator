096445b744040de62bc19fb454235bac7857df09
theV1rTual <arystandev@gmail.com>
2025-09-19 00:10:06 +0500

    FEATURE: implemented several methods

--- src/routers/videos-router.ts
+++ src/routers/videos-router.ts
@@ -1,15 +1,46 @@
 import {Router, Request, Response} from "express";
 import {videosRepository} from "../repositories/videos-repository";
-import {VideoType} from "../repositories/db";
+import {videosCollection, VideoType} from "../repositories/db";
 
 export const videosRouter = Router({})
 
 videosRouter.get('/', async (req: Request, res: Response) => {
   const videos = await videosRepository.getVideos()
-  res.send(videos)
+  res.status(200).send(videos)
 })
 
 videosRouter.get('/:id', async (req: Request, res: Response) => {
   const videos = await videosRepository.getVideos();
   videos.filter((value: VideoType) => value.id.toString() == req.params.id )
 })
+
+videosRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const user = req.body;
+
+    const result = await videosCollection.insertOne(user);
+
+    res.status(201).send(user);
+
+  }
+  catch (err) {
+    res.status(400);
+  }
+})
+
+videosRouter.delete('/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const videos = await videosRepository.getVideos();
+  if (videos.find((value: VideoType) => value.id.toString() === id)) {
+    const result = await videosCollection.deleteOne({id: new Object(req.params.id)})
+    res.sendStatus(204)
+  } else {
+    res.sendStatus(404)
+  }
+
+
+
+})
+
+
